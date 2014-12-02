13daeda6e296e99bcd58c7ee0fe9044e99063124
Justin Makaila <justinmakaila@gmail.com>
2014-12-02 16:31:48 -0500

    Initial Commit

---
+++ EmbeddedFrameworksContainer/EmbeddedFrameworksContainer.h
@@ -0,0 +1,19 @@
+//
+//  EmbeddedFrameworksContainer.h
+//  EmbeddedFrameworksContainer
+//
+//  Created by Justin Makaila on 12/2/14.
+//  Copyright (c) 2014 Justin Makaila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmbeddedFrameworksContainer.
+FOUNDATION_EXPORT double EmbeddedFrameworksContainerVersionNumber;
+
+//! Project version string for EmbeddedFrameworksContainer.
+FOUNDATION_EXPORT const unsigned char EmbeddedFrameworksContainerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmbeddedFrameworksContainer/PublicHeader.h>
+
+
