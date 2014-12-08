e4813a9f73bfd134991be1e1259349485293f3e2
Justin Makaila <justinmakaila@gmail.com>
2014-12-08 12:26:26 -0500

    Updated Cartfile, .lock, and added Object

---
+++ EmbeddedFrameworksContainer/Object.swift
@@ -0,0 +1,14 @@
+//
+//  Object.swift
+//  EmbeddedFrameworksContainer
+//
+//  Created by Justin Makaila on 12/8/14.
+//  Copyright (c) 2014 Justin Makaila. All rights reserved.
+//
+
+import Foundation
+import EmbeddedFrameworks
+
+class Object: NSObject {
+    var object: ExampleObject = ExampleObject()
+}
